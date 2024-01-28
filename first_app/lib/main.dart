edf625cd17f7b3900fd679434bf02cab96bcd389
Purabi <purbichoudhury06966@gmail.com>
2024-01-28 23:52:20 +0530

    practice flutter

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
         //Scaffold, Center, Text are widgets and MaterialApp is a constructor function
-        backgroundColor: Color.fromARGB(255, 152, 105, 233),
-        body: Center(
-          child: Text('Hello World!'),
+        // backgroundColor: Color.fromARGB(255, 152, 105, 233),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 160, 120, 230),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
         ),
       ),
     ),
